1c8d69f445c5b269a60fd09897fad738ef0aa156
Alexander <TheThirteenthLight@yandex.ru>
2024-03-03 14:56:14 +0300

    S

---
+++ src/main/java/p/parking/App.java
@@ -0,0 +1,10 @@
+package kur.project0;
+
+public class App {
+
+    public static void main(String[] args) {
+        {
+            HelloApplication.main(args);
+        }
+    }
+}
--- src/main/java/p/parking/HelloController.java
+++ src/main/java/p/parking/HelloController.java
@@ -1,14 +1,44 @@
-package p.parking;
+package kur.project0;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    Stage stage;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    AnchorPane scenePanel;
+    @FXML
+    AnchorPane scenePanel2;
+    @FXML
+    AnchorPane scenePanel1;
+
+    public void logout(ActionEvent event) {
+
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Do you want to save before exiting?: ");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scenePanel.getScene().getWindow();
+            System.out.println("Successfully logged out!");
+            stage.close();
+        }
+
+    }
+    public void MaximizeWindow(ActionEvent event) {
+        stage = (Stage) scenePanel.getScene().getWindow();
+        //stage.setMaximized(true);
+        stage.setWidth(1920);
+        stage = (Stage) scenePanel.getScene().getWindow();
+
+
     }
 }
