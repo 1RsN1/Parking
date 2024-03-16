fcb92840f0b74dc28ba7914e37b03136d2530d4c
1RsN1 <romnovs2005@gmail.com>
2024-03-16 18:45:20 +0300

    Romnov

--- src/main/java/p/parking/HelloApplication.java
+++ src/main/java/p/parking/HelloApplication.java
@@ -32,8 +32,6 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-
     public static void main(String[] args) {
         launch();
     }
