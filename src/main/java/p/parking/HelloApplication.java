6bc5df5f729aac5db6c738af7cba15470dc888e0
1RsN1 <romnovs2005@gmail.com>
2024-03-21 20:08:43 +0300

    JurTrev pane

--- src/main/java/p/parking/HelloApplication.java
+++ src/main/java/p/parking/HelloApplication.java
@@ -21,7 +21,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomeScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Arhiv.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 1440, 900);
         stage.initStyle(StageStyle.TRANSPARENT);
