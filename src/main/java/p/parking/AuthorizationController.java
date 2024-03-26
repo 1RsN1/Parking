f688f5c4a7d196a3e18251c954ad86f8db178a9b
1RsN1 <romnovs2005@gmail.com>
2024-03-26 17:48:08 +0300

    Сделана первоначальная авторизация

--- src/main/java/p/parking/AuthorizationController.java
+++ src/main/java/p/parking/AuthorizationController.java
@@ -2,10 +2,14 @@ package p.parking;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -40,7 +44,14 @@ public class AuthorizationController {
     private void checkLogin() throws IOException {
         HelloApplication hl = new HelloApplication();
         if(userName.getText().equals("admin") && password.getText().equals("admin")) {
-            hl.changeScene("HomeScene.fxml");
+            hl.changeSceneForAdmin();
+        } else if(userName.getText().isEmpty() && password.getText().isEmpty()) {
+            wrongLogIn.setText("Введите ваши данные");
+        } else {
+            wrongLogIn.setText("Неправильное имя или пароль");
+        }
+        if(userName.getText().equals("security") && password.getText().equals("security")) {
+            hl.changeSceneForSecurity();
         } else if(userName.getText().isEmpty() && password.getText().isEmpty()) {
             wrongLogIn.setText("Введите ваши данные");
         } else {
--- src/main/java/p/parking/HelloApplication.java
+++ src/main/java/p/parking/HelloApplication.java
@@ -26,8 +26,7 @@ public class HelloApplication extends Application {
         stg = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Authorization.fxml"));
         //HomeSceneMax or HomeScene
-
-        Scene scene = new Scene(fxmlLoader.load(), 600, 350);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 300);
         stage.initStyle(StageStyle.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
         stage.setResizable(false);
@@ -37,9 +36,30 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public void changeScene(String fxml) throws IOException {
-        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxml))), 1440, 900);
-        stg.getScene().setRoot(scene.getRoot());
+    public void changeSceneForAdmin() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomeScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 900);
+        Stage stage = new Stage();
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Project");
+        stage.getIcons().add(imgIconier);
+        stage.setScene(scene);
+        stg.close();
+        stage.show();
+    }public void changeSceneForSecurity() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomeSceneForSecurity.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 900);
+        Stage stage = new Stage();
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Project");
+        stage.getIcons().add(imgIconier);
+        stage.setScene(scene);
+        stg.close();
+        stage.show();
     }
 
     public static void main(String[] args) {
