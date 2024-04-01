cca438aeb057f7f86f293ef1f0fb54d93907705a
1RsN1 <romnovs2005@gmail.com>
2024-04-01 20:57:38 +0300

    Начата работа над оповещением

---
+++ src/main/java/p/parking/ParkingEvents.java
@@ -0,0 +1,13 @@
+package p.parking;
+
+public class ParkingEvents {
+
+    private int countOfCarInAccident;
+    public void incrementCountOfCarAccident() {
+        countOfCarInAccident++;
+    }
+    public int getCountOfCarAccident() {
+        return countOfCarInAccident;
+    }
+
+}
